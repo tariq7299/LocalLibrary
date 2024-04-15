c79b8538539d9e67e38dc833f347a669ca2a085e
tariq7299 <tr.sar77an78@gmail.com>
2024-04-15 14:32:39 +0200

    - Imported catalog routes in app.js - Now the app is working with defined routes in catalog.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ async function main() {
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const wiki = require("./routes/wiki.js");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
 
 const app = express();
 
@@ -33,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/wiki", wiki);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
